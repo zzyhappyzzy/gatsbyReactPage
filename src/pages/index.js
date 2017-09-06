ba1cde62353d73eff526f4499d0e60a2de1973c9
zhenzhaoyang <zzyhappyzzy@163.com>
2017-09-06 11:11:45 +0800

    init project

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello everybody</h1>
+    <p>Welcome to my site.</p>
+    <p>My name is momoer.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
